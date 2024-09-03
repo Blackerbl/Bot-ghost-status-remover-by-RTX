17c85e2afb1f988ec8934c3862160f9f8484d64f
Blackerbl <162997554+Blackerbl@users.noreply.github.com>
2024-09-03 13:31:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,71 @@
+const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const client = new Client({
-    intents: [
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildMessages,
-    ]
+  intents: Object.keys(GatewayIntentBits).map((a) => {
+    return GatewayIntentBits[a];
+  }),
 });
+const app = express();
+const port = 3000;
+app.get('/', (req, res) => {
+  res.send('YaY Your Bot Status Changed✨');
+});
+app.listen(port, () => {
+  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
+  console.log(`🔗 Powered By RTX`);
+});
+
+
+const statusMessages = ["Ben tatlı bir partner botuyummm","Seni İzliyorumm?","Sizinle Uğraşıyoum!","Kuru Kuru Kururin!","Hey Dikkatli Ol Gözüm Üzerinde! ","Minnak bir botum UwU <:focacomfy:1272421146544963646>","!Yardım izliyor..","Yardım için keke_km","Yardım için Wistyyx","https://discord.gg/Zz9ktZGgPy"];
+
+let currentIndex = 0;
+const channelId = '';
+
+async function login() {
+  try {
+    await client.login(process.env.TOKEN);
+    console.log(`\x1b[36m%s\x1b[0m`, `|    🐇 Logged in as ${client.user.tag}`);
+  } catch (error) {
+    console.error('Failed to log in:', error);
+    process.exit(1);
+  }
+}
+
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  client.user.setPresence({
+    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    status: 'dnd',
+  });
+
+  
+  const textChannel = client.channels.cache.get(channelId);
+
+  if (textChannel instanceof TextChannel) {
+   
+    textChannel.send(`Bot status is: ${currentStatus}`);
+  } else {
+
+  }
+
+  currentIndex = (currentIndex + 1) % statusMessages.length;
+}
 
 client.once('ready', () => {
-    console.log('Bot is online!');
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  updateStatusAndSendMessages();
+
+  setInterval(() => {
+    updateStatusAndSendMessages();
+  }, 10000);
 });
 
-client.on('guildMemberAdd', member => {
-    const channel = member.guild.channels.cache.get('1177975094581149791'); // Karşılama mesajı göndermek istediğiniz kanalın ID'si
-    if (!channel) return;
+login();
 
-    const welcomeMessage = {
-        content: `Aramıza Hoşgeldin <:3712zerotwoheartlove:1241389409585074206> ${member}  <@&1270293476398993418>`,
-        embeds: [
-            {
-                title: "Hoşgeldin",
-                description: `╭ »TENGOKU\n╎ ・<a:1794_sparkles:1241374294890643487>  ↦ ⁠・﹒ <#1177975089283731556> - kurallar Okumayı unutma!\n╎ ・ <a:4165_Hyped_ZeroTwo:1241374320912240701> ↦ ⁠⁠二・🍂﹒ <#1178051348537823355> - Kendini tanıtabilirsin..\n╎ ・<a:9770animepat:1251507026694246401>  ↦ ⁠ ⁠ <#1178266906256482385> - Renklerini Seç ve rollerini al!Hoşgeldin !\n╎ ・<a:lavendalove:1241483165403709581>  ↦ ⁠・﹒ <@&1241109140001001653>  - Partnerlik için geldiysen etiketleyebilirsin..\n╰ » Hadi Sana İyi Sohbetler`,
-                color: null,
-                author: {
-                    name: `${member.user.username} Sunucuya İniş Yaptı`
-                },
-                image: {
-                    url: "https://cdn.discordapp.com/attachments/1245412053506592870/1270294681334583361/15e7bdc6ebb30d2dd028ed52dfc5bbbb.gif?ex=66b32d9c&is=66b1dc1c&hm=d42b9a07f532cd8eb12aae3bd968cc9943b9d20d51d78ab58473bf22a49cc832&"
-                },
-                thumbnail: {
-                    url: "https://cdn.discordapp.com/attachments/1245412053506592870/1270294681334583361/15e7bdc6ebb30d2dd028ed52dfc5bbbb.gif?ex=66b32d9c&is=66b1dc1c&hm=d42b9a07f532cd8eb12aae3bd968cc9943b9d20d51d78ab58473bf22a49cc832&"
-                }
-            }
-        ],
-        username: "Rias",
-        avatar_url: "https://cdn.discordapp.com/attachments/1123948349326893076/1270294012200489032/rias-gremory-dxd-rias.gif?ex=66b32cfd&is=66b1db7d&hm=7ef57b68af3f06eaba053875edf28b1488bb85499298f2345fe8f573e36885b7&",
-        attachments: []
-    };
-
-    channel.send(welcomeMessage);
-});
-
-client.login(process.env.DISCORD_TOKEN);
